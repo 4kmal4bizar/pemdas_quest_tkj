8c8509be45c5d91c510396e626605ed1f045d526
4kmal4bizar <112526134+4kmal4bizar@users.noreply.github.com>
2022-09-07 08:47:15 +0700

    Add files via upload

---
+++ python7 - tkj4.py
@@ -0,0 +1,34 @@
+list_kosong = []
+list_interger = [1, 2, 3, 10, 100, 250,]
+list_string = ["rendra", "abyan", "udin", "naufal"]
+list_mix = [20, "arya", True, "sopyan"]
+
+print("list kosong:", list_kosong)
+print("list data interger:", list_interger)
+print("list data string:", list_string)
+print("list data mix:", list_mix)
+
+
+
+print("list kesatu list string:", list_string[0])
+print("data kedua list string:", list_string[1])
+print("data ketiga list string:", list_string[2])
+print("data keempat list string:", list_string[3])
+
+
+print("data kesatu list mix:", list_mix[0])
+print("data kedua list mix:", list_mix[1])
+print("data ketiga list mix:", list_mix[2])
+print("data keempat list mix:", list_mix[3])
+
+print("data kesatu list interger:", list_interger[0])
+print("data kedua list interger:", list_interger[1])
+print("data ketiga list interger:", list_interger[2])
+print("data keempat list interger:", list_interger[3])
+print("data kelima list interger:", list_interger[4])
+print("data keenam list interger:", list_interger[5])
+      
+
+
+
+      
