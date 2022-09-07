c13af10dff212b5e996bac4323a90de30206e7a5
4kmal4bizar <112526134+4kmal4bizar@users.noreply.github.com>
2022-09-07 13:52:42 +0700

    Add files via upload

---
+++ phyton8 - tkj4.py
@@ -0,0 +1,43 @@
+list_interger = [1, 2, 3, 10, 100, 250,]
+list_string = ["rendra", "abyan", "udin", "naufal"]
+list_mix = [20, "arya", True, "sopyan"]
+
+print("data sebelum diubah:", list_string)
+list_string[0] = "suroso"
+print("data setelah diubah:", list_string)
+print("data sebelum diubah:", list_string)
+list_string[1] = "jamil"
+print("data setelah diubah:", list_string)
+
+print("data sebelum diubah:", list_mix)
+list_mix[0] = 45
+print("data setelah diubah:", list_mix)
+print("data sebelum diubah:", list_mix)
+list_mix[1] = "dewa"
+print("data setelah diubah:", list_mix)
+print("data sebelum diubah:", list_mix)
+list_mix[2] = "false"
+print("data setelah diubah:", list_mix)
+print("data sebelum diubah:", list_mix)
+list_mix[3] = "deni"
+print("data setelah diubah:", list_mix)
+
+# perulangan for
+
+x = ["rendra", "abyan", "udin", "naufal"]
+for y in x:
+    print(y)
+print("/n")
+for i in list_string:
+    print(i)
+print("/n")
+for e in list_mix:
+    print(e)
+print("/n")
+for a in list_interger:
+    print(a)
+    
+
+
+
+
