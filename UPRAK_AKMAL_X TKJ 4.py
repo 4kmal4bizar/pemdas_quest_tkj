bcc707dabb9bf447d315a71fafdf269f6371d602
4kmal4bizar <112526134+4kmal4bizar@users.noreply.github.com>
2022-09-09 14:58:56 +0700

    Add files via upload

---
+++ UPRAK_AKMAL_X TKJ 4.py
@@ -0,0 +1,40 @@
+# rumus volume balok balok * panjang * tinggi
+
+panjang = 8
+lebar = 5
+tinggi = 6
+volume_b = panjang * lebar * tinggi
+print("volume balok adalah", volume_b)
+
+# rumus volume kubus sisi * sisi * sisi
+
+sisi = 8
+sisi = 5
+sisi = 7
+volume_k = sisi * sisi * sisi
+print("volume kubus adalah", volume_k)
+
+#rumus volume limas 1/3 * alas * tinggi
+
+alas = 8
+tinggi = 6
+volume_l = alas * tinggi * 1/3
+print("volume kubus adalah", volume_l)
+
+#rumus volume tabung 22/7 * r *r * t
+
+r = 2
+r = 4
+t = 25
+volume_t = r * r * t * 22/7
+print("volume tabung adalah", volume_t)
+
+#celcius ke reamor celcius * 4/5
+
+celcius = 20
+reamor = 4/5
+suhu = 20 * 4/5
+print("celcius dijadikan kereamor adalah", suhu)
+
+
+ 
